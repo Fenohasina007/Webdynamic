3f31a9aa96b6e64c9efa16d2f4d92509359f674f
Fenohasina007 <mamisoaandriatsitohaina@gmail.com>
2025-07-08 14:45:11 +0300

    nouvelle modification

--- BackCMS/src/services/siteData.service.ts
+++ BackCMS/src/services/siteData.service.ts
@@ -12,12 +12,13 @@ export class SiteDataService {
   async getSiteDataByName(nomSite: string, nomPage?: string): Promise<Site | null> {
     const query = this.siteRepository.createQueryBuilder('site')
       .where('site.NomSite = :nomSite', { nomSite })
-      .leftJoinAndSelect('site.composantes', 'composantes')
+      .leftJoinAndSelect('site.pages', 'pages') // Changé de 'composantes' à 'pages'
+      .leftJoinAndSelect('pages.composantes', 'composantes') // Ajout pour relier à Composante via Page
       .leftJoinAndSelect('composantes.contenus', 'contenus')
       .leftJoinAndSelect('contenus.personnalisations', 'personnalisations');
 
     if (nomPage) {
-      query.andWhere('composantes.NomPage = :nomPage', { nomPage });
+      query.andWhere('pages.NomPage = :nomPage', { nomPage }); // Filtre sur pages.NomPage
     }
 
     return await query.getOne();
